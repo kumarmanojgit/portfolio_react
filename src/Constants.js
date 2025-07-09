35aad9849d098b12914fbd84e958a956d606b478
kumarmanojgit <krmanoj72099@gmail.com>
2025-07-09 17:15:13 +0530

    skill section added

--- src/Constants.js
+++ src/Constants.js
@@ -8,7 +8,6 @@ import reduxLogo from "./assets/tech_logo/icons8-redux-24.png";
 
 import tailwindcssLogo from "./assets/tech_logo/icons8-tailwind-css-100.png";
 
-import materialuiLogo from "./assets/tech_logo/materialui.png";
 import bootstrapLogo from "./assets/tech_logo/icons8-bootstrap-48.png";
 import springbootLogo from "./assets/tech_logo/icons8-spring-boot-48.png";
 import nodejsLogo from "./assets/tech_logo/icons8-nodejs-48.png";
@@ -20,37 +19,33 @@ import cLogo from "./assets/tech_logo/icons8-c-48.png";
 import cppLogo from "./assets/tech_logo/icons8-c++.png";
 import javaLogo from "./assets/tech_logo/icons8-java-48.png";
 
-import typescriptLogo from "./assets/tech_logo/typescript.png";
 import gitLogo from "./assets/tech_logo/icons8-git-48.png";
 import githubLogo from "./assets/tech_logo/icons8-github-48.png";
 import vscodeLogo from "./assets/tech_logo/icons8-visual-studio-48.png";
 import postmanLogo from "./assets/tech_logo/icons8-postman-inc-24.png";
 
-import figmaLogo from "./assets/tech_logo/icons8-intellij-idea-48.png";
 import netlifyLogo from "./assets/tech_logo/icons8-netlify-24.png";
 
-import postgreLogo from "./assets/tech_logo/icons8-sql-48.png";
-
 // Experience Section Logo's
-import webverseLogo from "./assets/company_logo/webverse_logo.png";
-import agcLogo from "./assets/company_logo/agc_logo.png";
-import newtonschoolLogo from "./assets/company_logo/newtonschool_logo.png";
+import webverseLogo from "./assets/company_logo/images.jpg";
+import agcLogo from "./assets/company_logo/images.png";
+import newtonschoolLogo from "./assets/company_logo/newtonschool_logo.jpg";
 
 // Education Section Logo's
-import glaLogo from "./assets/education_logo/gla_logo.png";
-import bsaLogo from "./assets/education_logo/bsa_logo.png";
-import vpsLogo from "./assets/education_logo/vps_logo.png";
+// import glaLogo from "./assets/education_logo/gla_logo.png";
+// import bsaLogo from "./assets/education_logo/bsa_logo.png";
+// import vpsLogo from "./assets/education_logo/vps_logo.png";
 
 // Project Section Logo's
-import githubdetLogo from "./assets/work_logo/github_det.png";
-import csprepLogo from "./assets/work_logo/cs_prep.png";
-import movierecLogo from "./assets/work_logo/movie_rec.png";
-import taskremLogo from "./assets/work_logo/task_rem.png";
-import npmLogo from "./assets/work_logo/npm.png";
-import webverLogo from "./assets/work_logo/web_dig.png";
-import cmLogo from "./assets/work_logo/cm.png";
-import imagesearchLogo from "./assets/work_logo/image_search.png";
-import removebgLogo from "./assets/work_logo/remove_bg.png";
+// import githubdetLogo from "./assets/work_logo/github_det.png";
+// import csprepLogo from "./assets/work_logo/cs_prep.png";
+// import movierecLogo from "./assets/work_logo/movie_rec.png";
+// import taskremLogo from "./assets/work_logo/task_rem.png";
+// import npmLogo from "./assets/work_logo/npm.png";
+// import webverLogo from "./assets/work_logo/web_dig.png";
+// import cmLogo from "./assets/work_logo/cm.png";
+// import imagesearchLogo from "./assets/work_logo/image_search.png";
+// import removebgLogo from "./assets/work_logo/remove_bg.png";
 
 export const SkillsInfo = [
   {
@@ -85,10 +80,8 @@ export const SkillsInfo = [
       { name: "C", logo: cLogo },
       { name: "C++", logo: cppLogo },
       { name: "Java", logo: javaLogo },
-      { name: "Python", logo: pythonLogo },
-      { name: "C-Sharp", logo: csharpLogo },
+
       { name: "JavaScript", logo: javascriptLogo },
-      { name: "TypeScript", logo: typescriptLogo },
     ],
   },
   {
@@ -98,10 +91,8 @@ export const SkillsInfo = [
       { name: "GitHub", logo: githubLogo },
       { name: "VS Code", logo: vscodeLogo },
       { name: "Postman", logo: postmanLogo },
-      { name: "Compass", logo: mcLogo },
-      { name: "Vercel", logo: vercelLogo },
+
       { name: "Netlify", logo: netlifyLogo },
-      { name: "Figma", logo: figmaLogo },
     ],
   },
 ];
@@ -111,8 +102,8 @@ export const experiences = [
     id: 0,
     img: webverseLogo,
     role: "Fullstack Developer",
-    company: "Webverse Digital",
-    date: "April 2024 - Present",
+    company: "TDS Avenue CodeForge",
+    date: "Aug 2024 - Oct 2024",
     desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
     skills: [
       "HTML",
@@ -130,10 +121,10 @@ export const experiences = [
   {
     id: 1,
     img: agcLogo,
-    role: "Fullstack Engineer",
-    company: "Agumentik Group of Companies",
-    date: "July 2023 - March 2024",
-    desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    role: "Frontend Course",
+    company: "Udemy",
+    date: "July 2023 - Jan 2025",
+    desc: "Udemy offers top frontend courses like Colt Steele's Web Developer Bootcamp, Brad Traversy's HTML/CSS, and Maximilian's JavaScript and React guides. These cover HTML, CSS, JavaScript, React, and Sass with real-world projects. They're beginner-friendly, highly rated, and ideal for building strong frontend development skills from scratch to advanced.",
     skills: [
       "ReactJS",
       "Redux",
@@ -147,14 +138,14 @@ export const experiences = [
   {
     id: 2,
     img: newtonschoolLogo,
-    role: "Frontend Intern",
+    role: "Frontend Course",
     company: "Newton School",
-    date: "September 2021 - August 2022",
-    desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    date: "Feb 2025 - May 2025",
+    desc: "Newton School’s 3‑month Frontend Deep Dive—part of their Full Stack track—focuses on HTML, CSS, JavaScript, React, hooks, UI frameworks, testing, live mentoring, coding practice, and mock interviews. It's intensive, mentor‑driven, and career‑oriented. Alumni praise real‑time support, but some users question its depth and compare it to traditional engineering.",
     skills: ["HTML", "CSS", "Javascript", "Bootstrap", "Figma", "Material UI"],
   },
 ];
-
+/*
 export const education = [
   {
     id: 0,
@@ -294,4 +285,4 @@ export const projects = [
     github: "https://github.com/codingmastr/Image-Background-Remover",
     webapp: "https://removeyourbg.netlify.app/",
   },
-];
+];*/

97398c5238e5ae70bb55aa27980e680555e9b2b5
kumarmanojgit <krmanoj72099@gmail.com>
2025-07-11 17:13:18 +0530

    contact section

--- src/Constants.js
+++ src/Constants.js
@@ -32,9 +32,9 @@ import agcLogo from "./assets/company_logo/images.png";
 import newtonschoolLogo from "./assets/company_logo/newtonschool_logo.jpg";
 
 // Education Section Logo's
-// import glaLogo from "./assets/education_logo/gla_logo.png";
-// import bsaLogo from "./assets/education_logo/bsa_logo.png";
-// import vpsLogo from "./assets/education_logo/vps_logo.png";
+import glaLogo from "./assets/education_logo/logo-new2.png";
+import bsaLogo from "./assets/education_logo/download.jpg";
+import vicLogo from "./assets/education_logo/images.jpg";
 
 // Project Section Logo's
 import githubdetLogo from "./assets/work_logo/Screenshot 2025-07-10 114809.png";
@@ -148,45 +148,36 @@ export const experiences = [
     skills: ["HTML", "CSS", "Javascript", "Bootstrap", "Figma", "Material UI"],
   },
 ];
-/*
+
 export const education = [
   {
     id: 0,
-    img: glaLogo,
-    school: "GLA University, Mathura",
-    date: "Sept 2022 - July 2024",
-    grade: "7.81 CGPA",
-    desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
-    degree: "Master of Computer Applications - MCA",
+    img: bsaLogo,
+    school: "NIT Arunachal Pradesh, Arunachal Pradesh",
+    date: "Dec 2021 - May 2025",
+    grade: "73.67%",
+    desc: "I completed my Bachelor's degree in Computer Science B.Tech from NIT Arunachal Pradesh. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+    degree: "Bachelor of Technology - B.Tech (Computer Science)",
   },
   {
     id: 1,
-    img: bsaLogo,
-    school: "BSA College, Mathura",
-    date: "Sept 2018 - Aug 2021",
-    grade: "73.2%",
-    desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
-    degree: "Bachelor of Science - BSC (Computer Science)",
+    img: vicLogo,
+    school: "Bishnugarh Inter college, Bishnugarh",
+    date: "Apr 2019 - March 2020",
+    grade: "73.6%",
+    desc: "I completed my class 12 education from Bishnugarh Inter college, Bishnugarh Inter college, under the JAC board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+    degree: "JAC(XII) - PCM with Computer Science",
   },
   {
     id: 2,
-    img: vpsLogo,
-    school: "Vatsalya Public School Govardhan, Mathura",
+    img: glaLogo,
+    school: "Vivekananda Central school, Hazaribagh",
     date: "Apr 2017 - March 2018",
-    grade: "78%",
-    desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
-    degree: "CBSE(XII) - PCM with Computer Science",
-  },
-  {
-    id: 3,
-    img: vpsLogo,
-    school: "Vatsalya Public School Govardhan, Mathura",
-    date: "Apr 2015 - March 2016",
-    grade: "87.5%",
-    desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
+    grade: "86.2%",
+    desc: "I completed my class 10 education from Vivekananda Central school, Hazaribagh, under the CBSE board, where I studied Science with Computer.",
     degree: "CBSE(X), Science with Computer Application",
   },
-];*/
+];
 
 export const projects = [
   {
